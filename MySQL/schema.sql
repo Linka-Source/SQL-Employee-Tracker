692b0c28424f5c5b7b03c5a0810caeaf26b0494f
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-05 13:44:33 +0930

    created other two tables

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -3,6 +3,20 @@ create database employeeTracker_db;
 
 use employeeTracker_db;
 
+create table department (
+	id int auto_increment not null,
+	deptName varchar(30) not null,
+    primary key(id)
+);
+
+create table role_in_dept (
+	id int auto_increment not null,
+    title varchar(30) not null,
+    salary decimal null,
+    department_id int not null,
+    primary key(id)
+);
+
 create table employee (
 	id int auto_increment not null,
     first_name varchar(30) not null,
