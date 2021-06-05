bd225460ad16ed1d0143084e12856b34dc98ae7c
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-05 13:50:00 +0930

    added dept values

---
+++ MySQL/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (deptName) VALUES ("Community Development");
+INSERT INTO department (deptName) VALUES ("Property & Assets");
+INSERT INTO department (deptName) VALUES ("Engineering");
+INSERT INTO department (deptName) VALUES ("People & Culture");
+
