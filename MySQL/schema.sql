96212e2bd1378e1e43af6b62fff90e106e574424
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-05 19:48:27 +0930

    added seed info into schema and deleted seeds file

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -25,3 +25,20 @@ create table employee (
     manager_id int null,
     primary key(id)
 );
+
+INSERT INTO department (deptName) VALUES ("Community Development");
+INSERT INTO department (deptName) VALUES ("Property & Assets");
+INSERT INTO department (deptName) VALUES ("Engineering");
+INSERT INTO department (deptName) VALUES ("People & Culture");
+
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Building Surveyor",90000,1);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Planner",90000,1);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Administrator",65000,2);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Project Manager",125000,2);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Structural Engineer",110000,3);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Traffic Engineer",100000,3);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Talent Recruiter",70000,4);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Performance Manager",85000,4);
+
+INSERT INTO employee (first_name,last_name,role_id) VALUES ("Kevin","Bacon",1);
+INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ("Marky","Mark",5,1)
