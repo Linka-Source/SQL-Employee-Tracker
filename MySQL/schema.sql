9932cdc36cb99535e300e1c437c4684464d7063f
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-05 13:28:45 +0930

    added drop database and employee table

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -1,3 +1,13 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
 create database employeeTracker_db;
 
 use employeeTracker_db;
+
+create table employee (
+	id int auto_increment not null,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int not null,
+    manager_id int null,
+    primary key(id)
+);
