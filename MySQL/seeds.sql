6fa03695b70a1443d19a2054f2023e0cf7cd46b7
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-05 13:58:27 +0930

    added role values

--- MySQL/seeds.sql
+++ MySQL/seeds.sql
@@ -3,3 +3,11 @@ INSERT INTO department (deptName) VALUES ("Property & Assets");
 INSERT INTO department (deptName) VALUES ("Engineering");
 INSERT INTO department (deptName) VALUES ("People & Culture");
 
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Building Surveyor",90000,1);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Planner",90000,1);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Administrator",65000,2);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Project Manager",125000,2);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Structural Engineer",110000,3);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Traffic Engineer",100000,3);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Talent Recruiter",70000,4);
+INSERT INTO role_in_dept (title,salary,department_id) VALUES ("Performance Manager",85000,4);
