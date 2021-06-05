8d25b5491e16843ee7933ee1eb44d37a734ddfb8
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-06-05 15:33:02 +0930

    created adding prompts

--- index.js
+++ index.js
@@ -76,7 +76,84 @@ const prompts = () => {
           });
     };
 
-          
+const addDept = () => {
+    inquirer
+    .prompt([
+        {
+            name: 'deptName',
+            type: 'input',
+            message: 'Please enter name of new department:',
+        }    
+    ])
+    .then((answer) => {
+        const query = `INSERT INTO department (deptName) VALUES ('${answer.deptName}')`;
 
-          
-  
+        connection.query(query,(err,res) => {
+            console.log("New department added successfully :)");
+            prompts();
+        });
+    })
+}
+
+const addRole = () => {
+    inquirer
+    .prompt([
+        {
+            name: 'roleName',
+            type: 'input',
+            message: 'Please enter new role name:',
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'Please enter salary value for this role:',
+        },
+        {
+            name: 'deptID',
+            type: 'input',
+            message: 'Please enter department ID new role is to be linked to:',
+        }
+    ])
+    .then((answer) => {
+        const query = `INSERT INTO position (title, salary, department_id) VALUES ('${answer.roleName}', ${answer.salary}, ${answer.deptID})`;
+
+        connection.query(query,(err,res) => {
+            console.log("New role added successfully :)");
+            prompts();
+        });
+    })
+}
+
+const addEmployee = () => {
+    inquirer
+    .prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'Please enter employee\'s first name:',
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'Please enter employee\'s last name:',
+        },
+        {
+            name: 'role_id',
+            type: 'input',
+            message: 'Please enter employee\'s role ID:',
+        },
+        {
+            name: 'manager_id',
+            type: 'input',
+            message: 'Please enter ID of manager employee reports to:',
+        }
+    ])
+    .then((answer) => {
+        const query = `INSERT INTO emplyee (first_name, last_name, role_id, manager_id) VALUES ('${answer.firstName}', '${answer.lastName}', ${answer.role_id}, ${answer.manager_id})`;
+
+        connection.query(query,(err,res) => {
+            console.log("New employee added successfully :)");
+            prompts();
+        });
+    })
+}
